4be6c0785aee1dfb2c5202dc3a071ec9ed011f98
Denys Kravchuk <63023273+luminousnow@users.noreply.github.com>
2020-12-07 01:09:51 +0200

    Updates

--- static/js/main.069dad9b.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpgoit-react-hw-02-phonebook"]=this["webpackJsonpgoit-react-hw-02-phonebook"]||[]).push([[0],{10:function(t,e,n){},11:function(t,e,n){"use strict";n.r(e);var o=n(0),c=n(1),r=n.n(c),s=n(3),i=n.n(s);var a=function(){return Object(o.jsx)("div",{className:"App"})};n(9),n(10);i.a.render(Object(o.jsx)(r.a.StrictMode,{children:Object(o.jsx)(a,{})}),document.getElementById("root"))}},[[11,1,2]]]);
-//# sourceMappingURL=main.069dad9b.chunk.js.map
