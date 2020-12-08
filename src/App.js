63e29189cc855ca438b28cc054f2b9ce51727488
Denys Kravchuk <63023273+luminousnow@users.noreply.github.com>
2020-12-08 22:29:35 +0200

    Приховує заголовок за відсутності контактів, видаляє тестову колекцію контактів, додає коментарі

--- src/App.js
+++ src/App.js
@@ -7,21 +7,18 @@ import Section from './components/Section/Section';
 
 class App extends Component {
   state = {
-    contacts: [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ],
+    contacts: [],
     filter: '',
   };
 
+  // видаляє контакт по кліку на кнопку
   deleteContact = contactId => {
     this.setState(prevState => ({
       contacts: prevState.contacts.filter(cont => cont.id !== contactId),
     }));
   };
 
+  // отримує параметри з Форми, додає ІД і формує повноцінний контакт
   getContactData = data => {
     const id = uuidv4();
     const contact = { id, ...data };
@@ -40,7 +37,7 @@ class App extends Component {
           <ContactForm getContactData={getContactData} />
         </Section>
         <Section>
-          <h2>Contacts</h2>
+          {this.state.contacts.length > 0 && <h2>Contacts</h2>}
           {/* <Filter /> */}
           <ContactList contactList={contacts} onDeleteContact={deleteContact} />
         </Section>
